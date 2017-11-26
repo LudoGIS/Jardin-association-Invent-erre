c55c540dcfb22cb1a3f4fa04475b76521916952a
LudoGIS <33979902+LudoGIS@users.noreply.github.com>
2017-11-26 11:04:06 +0100

    Add files via upload

---
+++ data/lignes_3.js
@@ -0,0 +1,7 @@
+var json_lignes_3 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Haie fruitière", "DESCRIPTIO": "<table><tr><td><\/td><td><img src=\"file:\/\/\/C:\\Users\\Jessi\\Desktop\\parc asso cande\\le parc\\ArpentGIS_20171014155436.jpg\" width=\"250\"><\/td><\/tr><tr><td><\/td><td>", "ALTITUDEMO": "Des petits arbres fruitiers se suivent pour former une haie fruitière, vous trouverez des pommes et des poires. La haie fruitière offre un avantage écologique pour la biodiversité, et demeure une source non négligeable de nourriture pour les oiseaux." }, "geometry": { "type": "LineString", "coordinates": [ [ 2.37779289, 48.994897899973218 ], [ 2.3777939, 48.994897919973212 ], [ 2.37779381, 48.99489766997322 ], [ 2.37779419, 48.994897539973223 ], [ 2.37778964, 48.994899319973207 ], [ 2.37778122, 48.994901939973218 ], [ 2.377775974809281, 48.994903866021041 ], [ 2.37776398, 48.994907179973225 ], [ 2.37775515, 48.99490937997323 ], [ 2.37774524, 48.994911229973233 ], [ 2.37773453, 48.994914039973217 ], [ 2.37772761899951, 48.99491551443861 ], [ 2.37771323, 48.994918549973242 ], [ 2.377700964294413, 48.99492031039739 ], [ 2.377692261706839, 48.994922137330768 ], [ 2.37768503, 48.994924069973251 ], [ 2.377683937212248, 48.994924631799996 ] ] } }
+]
+}
